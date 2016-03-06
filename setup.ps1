20abb95441bbd10e240e26bfe803426153e37e38
zaki-yama <shingoyamazaki00@gmail.com>
2016-03-07 01:34:54 +0900

    いくつかファイルをインストールしてみる

--- setup.ps1
+++ setup.ps1
@@ -1,1 +1,10 @@
-echo "hello, world"
+ipmo international
+Set-WinSystemLocale ja-JP
+
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install -y notepadplusplus
+choco install -y tortoisesvn
+choco install -y git.install
+
+Restart-Computer -Wait
