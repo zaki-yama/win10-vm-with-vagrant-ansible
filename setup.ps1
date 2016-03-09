ab5c2e109359640befd0c3228473ba0b4cc69cec
zaki-yama <shingoyamazaki00@gmail.com>
2016-03-10 01:49:53 +0900

    ps1側は不要になった

--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,2 @@
 ipmo international
 Set-WinSystemLocale ja-JP
-
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-
-choco install -y notepadplusplus
-choco install -y tortoisesvn
-choco install -y git.install
-
-Restart-Computer -Wait
